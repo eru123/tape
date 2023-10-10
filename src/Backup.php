56040085026664d1586c2d8ea77c7ee96400ae09
eru123 <yeoligoakino@gmail.com>
2023-10-11 07:32:36 +0800

    test

--- src/Backup.php
+++ src/Backup.php
@@ -8,64 +8,70 @@ use eru123\venv\VirtualEnv;
 
 final class Backup
 {
-    final static function create(array $config, array $options = [], array $providers): callable
+    // final static function create(array $config): callable
+    // {
+    //     $provider = VirtualEnv::get($config, 'provider', false);
+    //     $providers['invalid'] = [
+    //         'class' => Provider\Invalid::class,
+    //     ];
+
+    //     $providers['none'] = [
+    //         'class' => Provider\None::class,
+    //     ];
+
+    //     if (!$provider || !isset($providers[$provider])) {
+    //         $provider = 'invalid';
+    //     }
+
+    //     return function () use ($provider, $providers, $config, $options) {
+    //         $datetime = DateTime::createFromFormat('U', Daemon::$time);
+    //         $cron = VirtualEnv::get($config, 'cron', false);
+    //         $crontab = VirtualEnv::get($config, 'crontab', false);
+    //         $cronjob = VirtualEnv::get($config, 'cronjob', false);
+
+    //         if (!$cron && !$crontab && !$cronjob) {
+    //             return;
+    //         }
+
+    //         if ($cron && !Crontab::match($cron, $datetime)) {
+    //             return;
+    //         } else if ($crontab && !Crontab::match($crontab, $datetime)) {
+    //             return;
+    //         } else if ($cronjob && !Crontab::match($cronjob, $datetime)) {
+    //             return;
+    //         }
+
+    //         try {
+    //             ob_start();
+    //             echo 'matched';
+    //             // $backup = static::do_backup($config, $options);
+    //             $output = ob_get_clean();
+    //         } catch (Throwable $e) {
+    //             ob_end_clean();
+    //             $output = $e->getMessage() . PHP_EOL . $e->getTraceAsString();
+    //         }
+    //         $lines = explode(PHP_EOL, trim($output));
+    //         $date = DateTime::createFromFormat('U', Daemon::$time)->format('Y-m-d H:i:s');
+    //         $provider = VirtualEnv::get($config, 'provider', 'unknown');
+    //         $provider = addslashes($provider);
+    //         $name = VirtualEnv::get($config, 'name', '');
+    //         $name = addslashes($name);
+    //         $title = trim("$name ($provider)");
+    //         foreach ($lines as $line) {
+    //             echo "[$date] $title: $line" . PHP_EOL;
+    //         }
+    //     };
+    // }
+    final static function createCallback($id): callable
     {
-        $provider = VirtualEnv::get($config, 'provider', false);
-        $providers['invalid'] = [
-            'class' => Provider\Invalid::class,
-        ];
-
-        $providers['none'] = [
-            'class' => Provider\None::class,
-        ];
-
-        if (!$provider || !isset($providers[$provider])) {
-            $provider = 'invalid';
-        }
-
-        return function () use ($provider, $providers, $config, $options) {
-            $datetime = DateTime::createFromFormat('U', Daemon::$time);
-            $cron = VirtualEnv::get($config, 'cron', false);
-            $crontab = VirtualEnv::get($config, 'crontab', false);
-            $cronjob = VirtualEnv::get($config, 'cronjob', false);
-            
-            if (!$cron && !$crontab && !$cronjob) {
-                return;
-            }
-            
-            if ($cron && !Crontab::match($cron, $datetime)) {
-                return;
-            } else if ($crontab && !Crontab::match($crontab, $datetime)) {
-                return;
-            } else if ($cronjob && !Crontab::match($cronjob, $datetime)) {
-                return;
-            }
-
-            try {
-                ob_start();
-                echo 'matched';
-                // $backup = static::do_backup($config, $options);
-                $output = ob_get_clean();
-            } catch (Throwable $e) {
-                ob_end_clean();
-                $output = $e->getMessage() . PHP_EOL . $e->getTraceAsString();
-            }
-            $lines = explode(PHP_EOL, trim($output));
-            $date = DateTime::createFromFormat('U', Daemon::$time)->format('Y-m-d H:i:s');
-            $provider = VirtualEnv::get($config, 'provider', 'unknown');
-            $provider = addslashes($provider);
-            $name = VirtualEnv::get($config, 'name', '');
-            $name = addslashes($name);
-            $title = trim("$name ($provider)");
-            foreach ($lines as $line) {
-                echo "[$date] $title: $line" . PHP_EOL;
-            }
-        };
+        $logfile = venv('config.log_path', '/app/app.log');
+        $runner = venv('config.runner', '/app/runner');
+        return fn() => Cmd::cmd("/bin/env php $runner $id >> $logfile 2>&1 &");
     }
 
-    final static function runner()
+    final static function runner($id)
     {
-        return fn() => false;
+
     }
 
     final static function do_backup($config, $options): false|string|array
@@ -91,7 +97,7 @@ final class Backup
 
         $backups_dir = venv('config.backups_dir', '/app/backups');
         $backup_file_unsafe = Fs::joinUnsafe($backups_dir, $name);
-        
+
         Fs::mkdir($backups_dir);
         if (file_exists($backup_file_unsafe)) {
             $backup_file_unsafe2 = Fs::joinUnsafe($backups_dir, $name . '.' . Daemon::$time . '.bak');
