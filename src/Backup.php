917eb0c1ad8fce26733ca8ac20071e8ebb53a1e9
eru123 <yeoligoakino@gmail.com>
2023-10-13 07:32:38 +0800

    backup: improvements

--- src/Backup.php
+++ src/Backup.php
@@ -5,21 +5,21 @@ namespace App;
 use DateTime;
 use Exception;
 use ZipArchive;
-use eru123\venv\VirtualEnv;
+use eru123\venv\VirtualEnv as A;
 
 final class Backup
 {
     final static function createCallback($id): callable
     {
         return function () use ($id) {
-            $datetime = DateTime::createFromFormat('U', time());
             $config = venv('config.data.backups.' . $id, []);
 
-            $cron = VirtualEnv::get($config, 'cron', false);
-            $crontab = VirtualEnv::get($config, 'crontab', false);
-            $cronjob = VirtualEnv::get($config, 'cronjob', false);
+            $cron = A::get($config, 'cron', false);
+            $crontab = A::get($config, 'crontab', false);
+            $cronjob = A::get($config, 'cronjob', false);
 
-            $date = $datetime->format('Y-m-d H:i:s');
+            $date = date('Y-m-d H:i:s');
+            $datetime = DateTime::createFromFormat('Y-m-d H:i:s', $date);
 
             if (!$cron && !$crontab && !$cronjob) {
                 echo "[$date] $id: no crontab found" . PHP_EOL;
@@ -44,23 +44,21 @@ final class Backup
 
     final static function info($id, $message)
     {
-        $datetime = DateTime::createFromFormat('U', time());
-        $date = $datetime->format('Y-m-d H:i:s');
+        $date = date('Y-m-d H:i:s');
         echo "[$date] INFO: $id: " . trim($message) . PHP_EOL;
     }
 
     final static function error($id, $message)
     {
-        $datetime = DateTime::createFromFormat('U', time());
-        $date = $datetime->format('Y-m-d H:i:s');
+        $date = date('Y-m-d H:i:s');
         echo "[$date] ERROR: $id: " . trim($message) . PHP_EOL;
     }
 
     final static function runner($id)
     {
         $backup = venv('config.data.backups.' . $id);
-        $name = VirtualEnv::get($backup, 'name', $id);
-        $provider_name = VirtualEnv::get($backup, 'provider');
+        $name = A::get($backup, 'name', $id);
+        $provider_name = A::get($backup, 'provider');
         $provider = venv('config.data.providers.' . $provider_name);
 
         if (!$backup) {
@@ -71,7 +69,7 @@ final class Backup
             throw new Exception("Provider {$provider_name} not found");
         }
 
-        $datetime = DateTime::createFromFormat('U', time());
+        $datetime = DateTime::createFromFormat('U', date('U'));
         $name = preg_replace_callback('/\%([a-zA-Z0-9]+|provider)\%/', function ($matches) use ($datetime, $provider_name) {
             if ($matches[1] === 'provider') {
                 return $provider_name;
@@ -88,24 +86,39 @@ final class Backup
         $title = trim("$name ($provider_name)");
         static::info($title, "Started");
 
-        $type = VirtualEnv::get($backup, 'type', false);
+        $type = A::get($backup, 'type', false);
         if (!in_array($type, ['file', 'directory', 'database'])) {
             static::error($title, "Invalid backup type");
         }
 
         $backup_obj = null;
 
-        if ($type === 'file') {
+        if (in_array(strtolower($type), ['file', 'directory', 'folder', 'dir'])) {
             $inc = [];
             $exc = [];
-            if (isset($backup['path'])) {
+
+            if (isset($backup['path']) && is_array($backup['path'])) {
+                $inc = array_merge($inc, $backup['path']);
+            } else if (isset($backup['path'])) {
                 $inc[] = $backup['path'];
             }
-            if (isset($backup['include'])) {
-                $inc = array_merge($inc, $backup['include']);
+
+            if (isset($backup['paths']) && is_array($backup['paths'])) {
+                $inc = array_merge($inc, $backup['paths']);
+            } else if (isset($backup['paths'])) {
+                $inc[] = $backup['paths'];
             }
-            if (isset($backup['exclude'])) {
-                $exc = $backup['exclude'];
+
+            if (isset($backup['include']) && is_array($backup['include'])) {
+                $inc = array_merge($inc, $backup['include']);
+            } else if (isset($backup['include'])) {
+                $inc[] = $backup['include'];
+            }
+
+            if (isset($backup['exclude']) && is_array($backup['exclude'])) {
+                $exc = array_merge($exc, $backup['exclude']);
+            } else if (isset($backup['exclude'])) {
+                $exc[] = $backup['exclude'];
             }
 
             $dir = venv('config.backups_dir', '/app/backups');
@@ -117,14 +130,9 @@ final class Backup
                 Fs::move($file, $file2);
             }
 
-            // create zip
             $zip = new ZipArchive();
             $zip->open($file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-
-            // add folder to zip with $name
             $zip->addEmptyDir($name);
-
-            // iterate files/dir, all files and dir will be added to zip as child of $name
             $added_file_count = 0;
             foreach ($inc as $value) {
                 $realpath = realpath($value);
@@ -132,14 +140,29 @@ final class Backup
                     static::error($title, "Invalid path: $value");
                     continue;
                 }
+
+                foreach ($exc as $rgx) {
+                    $rgx = str_replace('/', '\/', $rgx);
+                    $rgx = str_replace('.', '\.', $rgx);
+                    $rgx = str_replace('*', '.*', $rgx);
+                    $rgx = str_replace('?', '.', $rgx);
+                    $rgx = '/^' . $rgx . '$/';
+                    if (preg_match($rgx, $realpath)) {
+                        continue 2;
+                    }
+                }
+
                 $zip->addFile($realpath, $name . '/' . basename($realpath));
                 $added_file_count++;
             }
 
-            // close zip
+            $password = A::get($backup, 'password');
+            if ($password) {
+                $zip->setPassword($password);
+            }
+
             $zip->close();
 
-            // if no file added, delete the zip
             if ($added_file_count === 0) {
                 unlink($file);
                 static::error($title, "No file added in the backup. Exiting...");
@@ -152,56 +175,18 @@ final class Backup
                 'mime' => 'application/zip',
                 'size' => filesize($file),
             ];
+
+            static::info($title, "Backup file created");
         }
 
         if (!$backup_obj) {
             static::error($title, "Failed to create backup. Exiting...");
             return;
         }
-    }
 
-    final static function do_backup(int $id): false|string|array
-    {
-        return false;
-        // $type = VirtualEnv::get($config, 'type', false);
-
-        // if (!$type) {
-        //     return 'Invalid backup type';
-        // }
-
-        // return match ($type) {
-        //     'file' => static::do_backup_file($config, $options),
-        //     default => 'Invalid backup type',
-        // };
-    }
-
-    final static function do_backup_file($config, $options): false|string|array
-    {
-        echo 'backup file' . PHP_EOL;
-        $path = VirtualEnv::get($config, 'path', false);
-        $include = VirtualEnv::get($config, 'include', []);
-        $exclude = VirtualEnv::get($config, 'exclude', []);
-        $name = VirtualEnv::get($config, 'name', false);
-
-        $backups_dir = venv('config.backups_dir', '/app/backups');
-        $backup_file_unsafe = Fs::joinUnsafe($backups_dir, $name);
-
-        Fs::mkdir($backups_dir);
-        if (file_exists($backup_file_unsafe)) {
-            $backup_file_unsafe2 = Fs::joinUnsafe($backups_dir, $name . '.' . Daemon::$time . '.bak');
-            Fs::move($backup_file_unsafe, $backup_file_unsafe2);
+        if (!A::get($backup, 'persistent', false) && !A::get($backup, 'persist', false)) {
+            unlink($backup_obj['path']);
+            static::info($title, "Backup file deleted");
         }
-
-        $backup_file = Fs::join($backups_dir, $name);
-
-        if ($path) {
-            array_unshift($include, $path);
-        }
-
-        foreach ($include as $value) {
-            $realpath = realpath($value);
-        }
-
-        return false;
     }
 }
